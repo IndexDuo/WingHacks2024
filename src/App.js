fa40226bb3492c4ad23dc92d3c2995f49b35f239
IndexDDuo <jl.jing02123@gmail.com>
2024-04-06 02:58:18 +0000

    Apr 5, 2024, 10:58 PM

--- src/App.js
+++ src/App.js
@@ -10,15 +10,22 @@ import ResultScreen from './components/ResultScreen';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<WelcomeScreen />} />
-        <Route path="/mode-selection" element={<ModeSelectionScreen />} />
-        <Route path="/game" element={<GameScreen />} />
-        <Route path="/feedback" element={<FeedbackScreen />} />
-        <Route path="/results" element={<ResultScreen />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<WelcomeScreen />} />
+    //     <Route path="/mode-selection" element={<ModeSelectionScreen />} />
+    //     <Route path="/game" element={<GameScreen />} />
+    //     <Route path="/feedback" element={<FeedbackScreen />} />
+    //     <Route path="/results" element={<ResultScreen />} />
+    //   </Routes>
+    // </Router>
+    <div>
+      <WelcomeScreen />
+      <ModeSelectionScreen />
+      <GameScreen />
+      <FeedbackScreen />
+      <ResultScreen />
+    </div>
   );
 }
 

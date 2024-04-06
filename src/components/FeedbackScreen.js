7f5dcd7cfd69a2d5efede3af108701863b697033
lindsey-nielsen <122649641+lindsey-nielsen@users.noreply.github.com>
2024-04-06 19:58:23 -0400

    Add navigation and feedback functionality to FeedbackScreen and GameScreen components

--- src/components/FeedbackScreen.js
+++ src/components/FeedbackScreen.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import {useParams, useNavigate} from 'react-router-dom';
 
 const FeedbackScreen = () => {
-  return <div>Feedback Screen</div>;
-};
+  const navigate = useNavigate();
+  
+  // navigate to badges screens
+  const navigateToGameScreen = () => {
+    navigate("/game");
+  };
+  
+  const isCorrect = useParams();
+  console.log(isCorrect);
+
+  let feedbackMessage;
+  if (isCorrect === 'correct') {
+    feedbackMessage = "Correct!";
+  }
+  else {
+    feedbackMessage = "Incorrect!";
+  }
+
+    return (
+      <div>Feedback Screen
+        <h1>{feedbackMessage}</h1>
+        <button className="game-button" onClick={navigateToGameScreen}>
+          Back To Game
+        </button>
+      </div>
+    )
+  };
 
 export default FeedbackScreen;

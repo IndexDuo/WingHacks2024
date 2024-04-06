f66da639a2dd258ced22b9181e2819961b3b3ab1
IndexDDuo <jl.jing02123@gmail.com>
2024-04-06 06:10:52 +0000

    Apr 6, 2024, 2:10 AM

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,8 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+app.get('/data', (req, res) => {
+  res.send('send all data');
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

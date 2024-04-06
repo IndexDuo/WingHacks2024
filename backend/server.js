f8ba5f065d577ea425fa0104f2989b6828ed34f8
IndexDDuo <jl.jing02123@gmail.com>
2024-04-06 06:29:03 +0000

    Apr 6, 2024, 2:29 AM

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,12 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB...', err));
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB...', err));
 
 // Middleware
 app.use(express.json());
@@ -18,13 +21,14 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
+// '/data' route to send all data in JSON format
 app.get('/data', async (req, res) => {
   try {
-    // Get the database and collection names
-    const databaseName = 'CelebrityPhotos';
+    // Get the database and collection names from environment variables
+    const databaseName = process.env.DATABASE_NAME || 'CelebrityPhotos';
     const collectionName = 'KPopFaces';
 
-    // Fetch all data from the 'KPopFaces' collection in the 'CelebrityPhotos' database
+    // Fetch all data from the specified collection
     const data = await mongoose.connection.db.collection(collectionName).find({}).toArray();
 
     if (data.length === 0) {

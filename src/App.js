4018a1580d22d3c8adca61804da154fb32f6d46b
lindsey-nielsen <122649641+lindsey-nielsen@users.noreply.github.com>
2024-04-05 23:23:59 -0400

    Home and Kpop Screens

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/HomeScreen';
+import KpopScreen from './components/KpopScreen';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/kpop" element={<KpopScreen />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+function HomeScreen() {
+  const navigate = useNavigate();
+
+  // navigate to other screens
+  const navigateToKpopScreen = () => {
+    navigate('/kpop');
+  };
+
+  return (
+    <div className="App">
+      <header className = "App-header">
+        {/* logo here */}
+        <h1>BiasGuessr</h1>
+        <p>Can you guess the celebrity?</p>
+        <button onClick={navigateToKpopScreen}>K-POP</button>
+      </header>
+    </div>
+  );
+}
+
+export default HomeScreen;

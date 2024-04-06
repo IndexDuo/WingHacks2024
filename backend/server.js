09a2370d5d1d9580cb05a3b771d3274adbeeb7ce
IndexDDuo <jl.jing02123@gmail.com>
2024-04-06 06:36:42 +0000

    Apr 6, 2024, 2:36 AM

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // MongoDB connection
-mongoose.connect(`${process.env.MONGODB_URI}/CelebrityPhotos`, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  dbName: 'CelebrityPhotos'
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Could not connect to MongoDB...', err));

f24c95aca3ded9d061edb5bc4b5ecfd394f58e7d
IndexDDuo <jl.jing02123@gmail.com>
2024-04-06 06:01:39 +0000

    Apr 6, 2024, 2:01 AM

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB...', err));
+
+// Middleware
+app.use(express.json());
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

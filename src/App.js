627fb12ffdbeb8e40da2f88a924bd3e5b68f34bf
IndexDDuo <jl.jing02123@gmail.com>
2024-04-06 02:53:02 +0000

    Apr 5, 2024, 10:53 PM

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 // import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WelcomeScreen from './components/WelcomeScreen';
 import ModeSelectionScreen from './components/ModeSelectionScreen';
 import GameScreen from './components/GameScreen';
 import FeedbackScreen from './components/FeedbackScreen';
 import ResultScreen from './components/ResultScreen';
 
-
 function App() {
   return (
     <Router>
-    <Routes>
-      <Route path="/" exact component={WelcomeScreen} />
-      <Route path="/mode-selection" component={ModeSelectionScreen} />
-      <Route path="/game" component={GameScreen} />
-      <Route path="/feedback" component={FeedbackScreen} />
-      <Route path="/results" component={ResultScreen} />
-    </Routes>
-  </Router>
+      <Routes>
+        <Route path="/" element={<WelcomeScreen />} />
+        <Route path="/mode-selection" element={<ModeSelectionScreen />} />
+        <Route path="/game" element={<GameScreen />} />
+        <Route path="/feedback" element={<FeedbackScreen />} />
+        <Route path="/results" element={<ResultScreen />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+

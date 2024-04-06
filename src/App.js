ae4fa3f025275be93165728294eab5fd5fc80b2b
IndexDDuo <jl.jing02123@gmail.com>
2024-04-06 02:42:08 +0000

    Apr 5, 2024, 10:42 PM

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import WelcomeScreen from './components/WelcomeScreen';
 import ModeSelectionScreen from './components/ModeSelectionScreen';
 import GameScreen from './components/GameScreen';
@@ -12,14 +12,14 @@ import ResultScreen from './components/ResultScreen';
 function App() {
   return (
     <Router>
-    <Switch>
+    <Routes>
       <Route path="/" exact component={WelcomeScreen} />
       <Route path="/mode-selection" component={ModeSelectionScreen} />
       <Route path="/game" component={GameScreen} />
       <Route path="/feedback" component={FeedbackScreen} />
       <Route path="/results" component={ResultScreen} />
       {/* Add more routes as needed */}
-    </Switch>
+    </Routes>
   </Router>
   );
 }

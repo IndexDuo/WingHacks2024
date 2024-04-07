ffdb087c01dacf7595e05a67b92e110bc609bf51
ca764763 <112090392+ca764763@users.noreply.github.com>
2024-04-07 01:30:37 -0400

    minor result screen styling

--- src/components/ResultScreen.js
+++ src/components/ResultScreen.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import {useLocation, useNavigate} from 'react-router-dom';
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import "../styles/ResultScreen.css";
 
 const ResultScreen = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const {score, total} = location.state;
+  const { score, total } = location.state;
 
   const navigateToHomeScreen = () => {
     navigate("/");
   };
 
   return (
-    <div>
+    <div className="result">
       <h1>Game Over!</h1>
-      <h2>Your score: {score}/{total}</h2>
+      <h2>Here is your score:</h2>
+      <br />
+      <div className="circle">
+        <h2>
+          {score}/{total}
+        </h2>
+      </div>
+      <br />
+      <br />
       <button className="home-button" onClick={navigateToHomeScreen}>
         Back To Home
       </button>
     </div>
-  )
+  );
 };
 
 export default ResultScreen;

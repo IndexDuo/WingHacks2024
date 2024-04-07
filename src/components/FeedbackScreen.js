c66b2867121296fe80f289f607876e111fed76ac
ca764763 <112090392+ca764763@users.noreply.github.com>
2024-04-06 22:34:59 -0400

    Feedback styling

--- src/components/FeedbackScreen.js
+++ src/components/FeedbackScreen.js
@@ -1,34 +1,53 @@
-import React from 'react';
-import {useLocation, useNavigate} from 'react-router-dom';
+import React from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import CorrectImage from "../images/Correct.png";
+import IncorrectImage from "../images/Incorrect.png";
+import "../styles/FeedbackScreen.css";
 
 const FeedbackScreen = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  
+
   // navigate to badges screens
   const navigateToGameScreen = () => {
     navigate("/game");
   };
-  
-  const {isCorrect, name} = location.state;
+
+  const navigateToResults = () => {
+    navigate("/results");
+  };
+
+  const { isCorrect, name } = location.state;
   console.log("is correct", isCorrect);
 
   let feedbackMessage;
+  let feedbackImage;
   if (isCorrect) {
-    feedbackMessage = "Correct!";
-  }
-  else {
-    feedbackMessage = "Incorrect! The celebrity was " + name + ".";
+    feedbackMessage = <h1>"Correct!"</h1>;
+    feedbackImage = CorrectImage;
+  } else {
+    feedbackMessage = (
+      <div>
+        <h1>Incorrect!</h1>
+        <p>The celebrity was {name}.</p>
+      </div>
+    );
+    feedbackImage = IncorrectImage;
   }
 
-    return (
-      <div>Feedback Screen
-        <h1>{feedbackMessage}</h1>
-        <button className="game-button" onClick={navigateToGameScreen}>
-          Back To Game
-        </button>
-      </div>
-    )
-  };
+  return (
+    <div className="feedback">
+      {feedbackMessage}
+      <img src={feedbackImage} alt="thing" />
+      <button className="game-button" onClick={navigateToGameScreen}>
+        NEXT
+      </button>
+      <br />
+      <button className="exit-button" onClick={navigateToResults}>
+        EXIT
+      </button>
+    </div>
+  );
+};
 
 export default FeedbackScreen;

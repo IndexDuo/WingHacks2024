63436b0dcc6c96c37a2eada4ba50bff64f35c030
ca764763 <112090392+ca764763@users.noreply.github.com>
2024-04-07 04:07:23 -0400

    about

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,18 @@
 import React from "react";
+import star from "../images/Colored_Badge.png";
 
 const About = () => {
-    return <div>About Screen</div>;
+  return (
+    <div className="about">
+      <h1>About the app</h1>
+      <div className="para">
+        <p></p>
+      </div>
+      <div className="team">
+        <div className="member"></div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
